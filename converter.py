989d641b8147bd3becf2e49477478a13c9465e4c
Latasha Harwani <115960087+Tasha1403@users.noreply.github.com>
2024-03-23 06:39:27 +0800

    added rounding function
    added functional rounding for ceiling, floor, and truncate.

--- converter.py
+++ converter.py
@@ -31,6 +31,82 @@ def normalize(number, exponent):
         add_exp -= 1
     return int(number), exponent + add_exp
 
+# FUNCTION: rounding
+def rounding(length, sign_bit, number):
+    #calculate how many excess numbers
+    excess = length - 7
+    n = 1
+
+    #loop until user inputs a valid rounding type
+    while(n == 1):
+        print(" 1. C - Ceiling\n 2. F - Floor\n 3. Z - Round to Zero\n 4. N - Round to Nearest (Ties to Even)")
+        r_type = str(input("Enter rounding type: "))
+
+        #Ceiling
+        if(r_type == 'C' or r_type == 'c'):
+            #if number is positive
+            if(sign_bit == '0'):
+                number = number/(10**excess)
+                number += 1
+            #if number is negative
+            elif(sign_bit == '1'):
+                number = int(number/(10**excess))
+            #break out of loop
+            n = 0
+
+        #Floor
+        elif(r_type == 'F' or r_type == 'f'):
+            #if number is positive
+            if(sign_bit == '0'):
+                number = int(number/(10**excess))
+            #if number is negative
+            elif(sign_bit == '1'):
+                number = int(number/(10**excess))
+                number -= 1
+            #break out of loop
+            n = 0
+
+        #Round to Zero/Truncate
+        elif(r_type == 'Z' or r_type == 'z'):
+            number = int(number/(10**excess))
+            #break out of loop
+            n = 0
+
+        #Round to Nearest (Ties to Even)
+        elif(r_type == 'N' or r_type == 'n'):
+            #if number is positive
+           ''' if(sign_bit == '0'):
+                #if last digit is greater than 5
+                if(number[-1] > 5):
+                    number = int(number/(10**excess))
+                    number += 1
+                #if last digit is less than 5
+                elif(number[-1] < 5):
+                    number = int(number/(10**excess))
+                #if last digit is 5
+                elif(number[-1] == 5):
+                    number = int(number/(10**excess))
+                #break out of loop
+                n = 0
+            
+            #if number is negative
+            if(sign_bit == '1'):
+                #if last digit is greater than 5
+                if(number[-1] > 5):
+                    number = int(number/10)
+                    number -= 1
+                #if last digit is less than 5
+                elif(number[-1] < 5):
+                    number = int(number/10)
+                #if last digit is 5
+                elif(number[-1] == 5):
+                    number = int(number/10)   
+                #break out of loop
+                n = 0     '''
+        else:
+            print("Invalid Input. Please Try Again.")
+    
+    return number
 
 # FUNCTION: get e'
 def get_e_prime(exponent):
@@ -146,8 +222,13 @@ def decimal_32_floating_ponint_converter():
         else:
             number = int(number)
 
-        # TODO: check if numbber of digits > 7
+        # TODO: check if number of digits > 7
         # if yes, ask for preferred rounding method
+        length = len(str(number))
+    
+        if(length > 7):
+            number = rounding(length, sign_bit, number)
+            print("Rounded number: " + str(number))
 
         # make number a string
         number_str = str(number)
